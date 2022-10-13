a7bf76103089cbdbbcfd5441369e0e87300df298
Ralph Unrau <xralphunrau@gmail.com>
2022-10-13 13:58:42 -0700

    Adds header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import './styles/App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div>
-      Currency Control
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './styles/Header.scss';
+
+function Header() {
+  return (
+    <div>
+      Currency Control
+    </div>
+  )
+}
+
+export default Header;
