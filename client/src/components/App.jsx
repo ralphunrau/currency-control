678b25b1c53a6ce116db2f652c221bf2c2a39809
Ralph Unrau <xralphunrau@gmail.com>
2022-10-14 14:30:41 -0700

    Adds testing component to confirm data is being sent to the frontend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,10 +2,12 @@ import './styles/App.css';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
+import Testing from './Testing';
 
 function App() {
   return (
     <div>
+      {/* <Testing /> */}
       <Header />
       <Body />
       <Footer />
