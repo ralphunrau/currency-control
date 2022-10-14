497d170853ebcb039091d3a8317eb7e1cc59ee6d
Ralph Unrau <xralphunrau@gmail.com>
2022-10-14 14:03:33 -0700

    Seperates server and client

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import './styles/Body.scss';
+
+function Body() {
+  return (
+    <div className='body-main'>
+      <h2>Welcome to your savings!</h2>
+      <p1>Our goal at Currency Control is to assist you in making the best financial decisions for your future! Input your income and expenses to determine how much time is needed to save up for your next big purchase!</p1>
+    </div>
+  );
+}
+
+export default Body;
