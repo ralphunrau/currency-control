c42983dff6a08a003b07bea83e7678f6beaabb26
Ralph Unrau <xralphunrau@gmail.com>
2022-10-25 15:22:57 -0700

    Adds login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,8 +2,34 @@ import './styles/LoginForm.scss';
 
 function LoginForm() {
   return (
-    <div className='login-form'>
+    <div className='login-page'>
       <h2>Login Form</h2>
+      <div className='login-form'>
+        <div className='form-header'>
+          <text>Create a Currency Control account.</text>
+          <text>Get started on your personal money-saving journey!</text>
+        </div>
+        <div className='form-input'>
+          <text>First Name</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Last Name</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Email</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Password</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Confirm Password</text>
+          <input></input>
+        </div>
+      </div>
     </div>
   );
 }
