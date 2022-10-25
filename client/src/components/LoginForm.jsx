f97486869f06603ce9923f97a21684de07c1de08
Ralph Unrau <xralphunrau@gmail.com>
2022-10-25 16:00:13 -0700

    Adds submit button to user forms

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,19 +3,9 @@ import './styles/LoginForm.scss';
 function LoginForm() {
   return (
     <div className='login-page'>
-      <h2>Login Form</h2>
       <div className='login-form'>
         <div className='form-header'>
-          <text>Create a Currency Control account.</text>
-          <text>Get started on your personal money-saving journey!</text>
-        </div>
-        <div className='form-input'>
-          <text>First Name</text>
-          <input></input>
-        </div>
-        <div className='form-input'>
-          <text>Last Name</text>
-          <input></input>
+          <text>Log into your Currency Control account!</text>
         </div>
         <div className='form-input'>
           <text>Email</text>
@@ -23,12 +13,9 @@ function LoginForm() {
         </div>
         <div className='form-input'>
           <text>Password</text>
-          <input></input>
+          <input type='password'></input>
         </div>
-        <div className='form-input'>
-          <text>Confirm Password</text>
-          <input></input>
-        </div>
+        <button>Submit</button>
       </div>
     </div>
   );
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -2,8 +2,34 @@ import './styles/SignupForm.scss';
 
 function SignupForm() {
   return (
-    <div className='signup-form'>
-      <h2>Signup Form</h2>
+    <div className='signup-page'>
+      <div className='signup-form'>
+        <div className='form-header'>
+          <text>Create a Currency Control account.</text>
+          <text>Get started on your personal money-saving journey!</text>
+        </div>
+        <div className='form-input'>
+          <text>First Name</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Last Name</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Email</text>
+          <input></input>
+        </div>
+        <div className='form-input'>
+          <text>Password</text>
+          <input type='password'></input>
+        </div>
+        <div className='form-input'>
+          <text>Confirm Password</text>
+          <input type='password'></input>
+        </div>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
