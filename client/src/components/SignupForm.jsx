10a801aab6e79c3d86d1cc37751940ee2138a68b
Ralph Unrau <xralphunrau@gmail.com>
2022-11-01 23:42:06 -0700

    Fixes signup page not loading

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -4,12 +4,13 @@ function SignupForm(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(event.target[0].value)
     console.log('got to handle submit');
   }
 
   return (
     <div className='signup-page'>
-      <form className='signup-form' action='/user/signup' method='POST'>
+      <form className='signup-form' action='/user/signup' method='POST' onSubmit={handleSubmit}>
         <div className='form-header'>
           <label>Create a Currency Control account.</label>
           <label>Get started on your personal money-saving journey!</label>
