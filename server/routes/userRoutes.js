eabe0a5a9deeca73f3c0ce6c3c59a1d22954f64a
Ralph Unrau <xralphunrau@gmail.com>
2022-11-28 15:20:37 -0800

    Adds bcrypt as dependancy

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const { addNewUser, getUserByEmail } = require('../db/db');
 const { checkInputField } = require('../helpers/checkInputField');
