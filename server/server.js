4923d1339b543b1cd37bb84a11d465f8a381f574
Ralph Unrau <xralphunrau@gmail.com>
2022-11-30 18:19:44 -0800

    Configures session in server index

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+  origin: ["http://localhost:3002"],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(session({
+  key: 'userId',
+  secret: 'topSecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 60 * 60 * 24,
+  }
+}));
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/user', userRoutes);
