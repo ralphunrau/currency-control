3e6ef6f70affbf38267a0cbbba04dc5c2361f77d
Ralph Unrau <xralphunrau@gmail.com>
2022-11-30 18:05:21 -0800

    Resolves broken path

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,6 +1,9 @@
-require('dotenv').config({path: '../.env'});
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
+
+console.log(knexConfig);
 
 module.exports = require('knex')(knexConfig);
