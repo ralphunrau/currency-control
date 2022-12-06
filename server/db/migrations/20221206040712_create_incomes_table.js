590310ba547a7a3e334df030147ab756b1ed649d
Ralph Unrau <xralphunrau@gmail.com>
2022-12-05 20:21:33 -0800

    Removes ondelete

--- server/db/migrations/20221206040712_create_incomes_table.js
+++ server/db/migrations/20221206040712_create_incomes_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('incomes', (t) => {
     t.increments('id').primary();
     t.integer('user_id').unsigned().references('id').inTable('users').onDelete('cascade').notNullable();
-    t.string('category', 25).onDelete('cascade');
+    t.string('category', 25);
     t.date('date');
   })
 };
