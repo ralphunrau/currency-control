08f496d7532ba788c35e9c15f42ec7936afa0e88
Ralph Unrau <xralphunrau@gmail.com>
2022-12-06 23:57:36 -0800

    Moves MUI component to Transaction display

--- client/src/components/TransactionForm.jsx
+++ client/src/components/TransactionForm.jsx
@@ -1,27 +1,9 @@
 import './styles/TransactionForm.scss';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function TransactionForm(props) {
   return (
     <div className='transaction-form-main'>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            // value={age}
-            label="Age"
-            // onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
+      Input Your transactions here!
     </div>
   );
 }
