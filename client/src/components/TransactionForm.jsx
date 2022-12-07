4aa521da08c8fc23aed0e415c1440cc3388ec1c6
Ralph Unrau <xralphunrau@gmail.com>
2022-12-06 23:55:12 -0800

    Adds MUI dropdown component

--- client/src/components/TransactionForm.jsx
+++ client/src/components/TransactionForm.jsx
@@ -1,9 +1,27 @@
 import './styles/TransactionForm.scss';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function TransactionForm(props) {
   return (
     <div className='transaction-form-main'>
-      Input Your transactions here!
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            // value={age}
+            label="Age"
+            // onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
     </div>
   );
 }
