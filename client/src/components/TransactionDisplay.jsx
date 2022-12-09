377a4e12e1ce617d459ac159027f8488dedace42
Ralph Unrau <xralphunrau@gmail.com>
2022-12-08 23:22:47 -0800

    Adds app data to drop down form

--- client/src/components/TransactionDisplay.jsx
+++ client/src/components/TransactionDisplay.jsx
@@ -8,20 +8,24 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 function TransactionDisplay(props) {
   return (
     <div className='transaction-display-main'>
-            <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                // value={age}
-                label="Age"
-                // onChange={handleChange}
-              >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+      <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Select a category</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          label="expense-category"
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Food</MenuItem>
+          <MenuItem value={20}>Clothing</MenuItem>
+          <MenuItem value={30}>Transportation</MenuItem>
+          <MenuItem value={40}>Housing</MenuItem>
+          <MenuItem value={50}>Utilities</MenuItem>
+          <MenuItem value={60}>Medical</MenuItem>
+          <MenuItem value={70}>Debt Payments</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
